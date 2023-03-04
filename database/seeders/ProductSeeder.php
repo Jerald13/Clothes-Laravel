3f6586e29e09781bab10a8032f8d0718ba1f48e4
Liang <zhongliang_01@hotmail.com>
2023-03-04 20:33:05 +0800

    Added Product data, dynamic product dispaly

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,38 +16,151 @@ class ProductSeeder extends Seeder
         //
         DB::table("products")->insert([
             [
-                "name" => "Oppo mobile",
-                "price" => "300",
+                "name" => "Esprit Ruffle Shirt",
+                "price" => "16.64",
                 "description" =>
-                    "A smartphone with 8gb ram and much more feature",
-                "category" => "mobile",
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
                 "gallery" =>
-                    "https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBOPPO-A52-6-GFUTU6297453D3D253C/1592019058170_0..png",
+                    "images/product-01.jpg",
             ],
             [
-                "name" => "Panasonic Tv",
-                "price" => "400",
-                "description" => "A smart tv with much more feature",
-                "category" => "tv",
+                "name" => "Herschel supply",
+                "price" => "35.31",
+                "description" =>
+                    "Good Nice T",
+                "category" => "1",
                 "gallery" =>
-                    "https://i.gadgets360cdn.com/products/televisions/large/1548154685_832_panasonic_32-inch-lcd-full-hd-tv-th-l32u20.jpg",
+                    "images/product-02.jpg",
             ],
             [
-                "name" => "Soni Tv",
-                "price" => "500",
-                "description" => "A tv with much more feature",
-                "category" => "tv",
+                "name" => "Only Check Trouser",
+                "price" => "25.50",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "2",
                 "gallery" =>
-                    "https://4.imimg.com/data4/PM/KH/MY-34794816/lcd-500x500.png",
+                    "images/product-03.jpg",
             ],
             [
-                "name" => "LG fridge",
-                "price" => "200",
-                "description" => "A fridge with much more feature",
-                "category" => "fridge",
+                "name" => "Classic Trench Coat",
+                "price" => "75.00",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
                 "gallery" =>
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFx-2-wTOcfr5at01ojZWduXEm5cZ-sRYPJA&usqp=CAU",
+                    "images/product-04.jpg",
             ],
+            [
+                "name" => "Front Pocket Jumper",
+                "price" => "34.75",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-05.jpg",
+            ],
+            [
+                "name" => "Vintage Inspired Classic",
+                "price" => "93.20",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "5",
+                "gallery" =>
+                    "images/product-06.jpg",
+            ],
+            [
+                "name" => "Shirt in Stretch Cotton",
+                "price" => "63.16",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-07.jpg",
+            ],
+            [
+                "name" => "Pieces Metallic Printed",
+                "price" => "18.96",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-08.jpg",
+            ],
+            [
+                "name" => "Converse All Star Hi Plimsolls",
+                "price" => "75.00",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "4",
+                "gallery" =>
+                    "images/product-09.jpg",
+            ],
+            [
+                "name" => "Femme T-Shirt In Stripe",
+                "price" => "25.85",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-10.jpg",
+            ],
+            [
+                "name" => "Herschel supply 2",
+                "price" => "63.15",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "2",
+                "gallery" =>
+                    "images/product-11.jpg",
+            ],
+            [
+                "name" => "Herschel supply 3",
+                "price" => "38.55",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "2",
+                "gallery" =>
+                    "images/product-12.jpg",
+            ],
+            [
+                "name" => "T-Shirt with Sleeve",
+                "price" => "18.49",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-13.jpg",
+            ],
+            [
+                "name" => "Pretty Little Thing",
+                "price" => "54.79",
+                "description" =>
+                    "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-14.jpg",
+            ],
+            [
+                "name" => "Mini Silver Mesh Watch",
+                "price" => "86.85",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "5",
+                "gallery" =>
+                    "images/product-15.jpg",
+            ],
+            [
+                "name" => "quare Neck Back",
+                "price" => "29.64",
+                "description" =>
+                   "Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.",
+                "category" => "1",
+                "gallery" =>
+                    "images/product-16.jpg",
+            ]
+
+
         ]);
     }
 }
